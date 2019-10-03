791356334f862eae418a9f5b09562c117a75fef2
salas <salas@M507.usbcali.edu.co>
2019-10-02 19:02:26 -0500

    clase 3

--- wsjfiayo/bank-logic/src/test/java/co/edu/usbcali/bank/spring/UsuarioSpringTest.java
+++ wsjfiayo/bank-logic/src/test/java/co/edu/usbcali/bank/spring/UsuarioSpringTest.java
@@ -1,53 +1,46 @@
-package co.edu.usbcali.bank.jpa;
+package co.edu.usbcali.bank.spring;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
-import co.edu.usbcali.bank.domain.Cliente;
-import co.edu.usbcali.bank.domain.TipoDocumento;
+
 import co.edu.usbcali.bank.domain.TipoUsuario;
 import co.edu.usbcali.bank.domain.Usuario;
 
-class UsuarioJPATest {
+
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration("/applicationContext.xml")
+@Rollback(false)
+class UsuarioSpringTest {
 
 	private final static String usu_usuario = "jfiayo";
-	private final static Logger log=LoggerFactory.getLogger(UsuarioJPATest.class);
+	private final static Logger log=LoggerFactory.getLogger(UsuarioSpringTest.class);
 	
-	EntityManagerFactory entityManagerFactory = null;
-	EntityManager entityManager = null;
-
-	@BeforeEach
-	void beforeEach() {
-		entityManagerFactory = Persistence.createEntityManagerFactory("bank-logic");
-		entityManager = entityManagerFactory.createEntityManager();
-
-	}
-
-	@AfterEach
-	void afterEach() {
-		entityManagerFactory.close();
-		entityManager.close();
-
-	}
+	
+	@PersistenceContext
+	EntityManager entityManager;
 
 	@Test
 	@DisplayName("save")
+	@Transactional(readOnly = false,propagation = Propagation.REQUIRED)
 	void aTest() {
 
 		assertNotNull(entityManager, "EL entityManager NULL");
@@ -62,13 +55,12 @@ class UsuarioJPATest {
 		TipoUsuario tipoUsuario = entityManager.find(TipoUsuario.class, 1L);
 		assertNotNull(tipoUsuario, "Tipo usuario no existe");
 		usuario.setTipoUsuario(tipoUsuario);
-		entityManager.getTransaction().begin();
 		entityManager.persist(usuario);
-		entityManager.getTransaction().commit();
-		
+				
 	}
 	@Test
 	@DisplayName("finById")
+	@Transactional(readOnly = false,propagation = Propagation.REQUIRED)
 	void bTest() {
 
 		assertNotNull(entityManager, "EL entityManager NULL");
@@ -79,6 +71,7 @@ class UsuarioJPATest {
 
 	@Test
 	@DisplayName("update")
+	@Transactional(readOnly = false,propagation = Propagation.REQUIRED)
 	void cTest() {
 
 		assertNotNull(entityManager, "EL entityManager NULL");
@@ -87,24 +80,24 @@ class UsuarioJPATest {
 
 		usuario.setActivo("N");
 
-		entityManager.getTransaction().begin();
 		entityManager.merge(usuario);
-		entityManager.getTransaction().commit();
+		
 		
 
 	}
 
 	@Test
 	@DisplayName("delete")
+	@Transactional(readOnly = false,propagation = Propagation.REQUIRED)
 	void dTest() {
 
 		assertNotNull(entityManager, "EL entityManager NULL");
 		Usuario usuario= entityManager.find(Usuario.class, usu_usuario);
 		assertNotNull(usuario, "Usuario con Id:" + usu_usuario + " No existe");
 
-		entityManager.getTransaction().begin();
+	
 		entityManager.remove(usuario);
-		entityManager.getTransaction().commit();
+		
 		
 
 	}
@@ -114,6 +107,7 @@ class UsuarioJPATest {
 	
 	@Test
 	@DisplayName("list")
+	@Transactional(readOnly = true)
 	void eTest() {
 
 		assertNotNull(entityManager, "EL entityManager NULL");
@@ -132,6 +126,5 @@ class UsuarioJPATest {
      		log.info(usuario.getIdentificacion().toString());     		
      	});
 	}
-
 
 }
